b4d4946fc73dd2fa38efbb3c294bd95f7a2ab783
jon-snow <sarna.kunal07@gmail.com>
2018-12-20 19:59:32 +0530

    about section and project

---
+++ js/app.js
@@ -0,0 +1,33 @@
+particlesJS(
+  "particles-js",
+
+  {
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 1000
+        }
+      },
+      color: {
+        value: "#fff"
+      },
+      shape: {
+        type: "image",
+        image: {
+          src: "./react-logo.png"
+        }
+      },
+      move: {
+        enable: true,
+        speed: 1
+      },
+      line_linked: {
+        enable: true,
+        color: "#61DAFB",
+        width: 5
+      }
+    }
+  }
+);
